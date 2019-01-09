cfc0d066ea2f25d35085c8acbd89418522a310eb
hehuijie <m18810146471@163.com>
2019-01-09 11:56:04 +0800

    日期格式、一周的起始时间、一个月的起始时间 的获取

--- utils/util.js
+++ utils/util.js
@@ -8,13 +8,59 @@ const formatTime = date => {
 
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
-
+// 日期 如：2019-01-09
 const formatDate = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
 
   return [year, month, day].map(formatNumber).join('-')
+}
+// 获取 一周的开始日期
+const formatWeekFirstDay = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const weekDay = date.getDay()
+  return [year, month, day - weekDay + 1 ].map(formatNumber).join('-')
+}
+// 获取 一周的结束日期
+const formatWeekLastDay = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const weekDay = date.getDay()
+  // new Date(tYear, tMonth, tDay + (6 - tWeekDay) + 1)
+  return [year, month, day + (6 - weekDay) + 1].map(formatNumber).join('-')
+}
+
+// 获取 某月的开始日期
+const formatMonthFirstDay = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  return [year, month, 1].map(formatNumber).join('-')
+}
+
+// 获取 某月的结束日期
+const formatMonthLastDay = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  return [year, month, formatMonthDays(date)].map(formatNumber).join('-')
+}
+
+// 获取某月份的天数
+const formatMonthDays = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth()
+
+  const monthDate = new Date(year, month, 1);
+  const nextMonthDate = new Date(year, month + 1, 1);
+
+  return (nextMonthDate - monthDate) / (1000 * 60 * 60 * 24)
 }
 
 const formatNumber = n => {
@@ -29,5 +75,10 @@ const check = res => {
 module.exports = {
   formatTime: formatTime,
   formatDate: formatDate,
+  formatWeekFirstDay: formatWeekFirstDay,
+  formatWeekLastDay: formatWeekLastDay,
+  formatMonthFirstDay: formatMonthFirstDay,
+  formatMonthLastDay: formatMonthLastDay,
+  formatMonthDays: formatMonthDays,
   check : check
 }
